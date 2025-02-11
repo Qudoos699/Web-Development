4b1dd21be7360224daf5e914dde3e5a9a7736391
Qudoos699 <qudoosabdul699@gmail.com>
2025-02-11 17:04:08 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// let a = 5 
+// var name = "john"
+// const pi = 3.142
+// // console.log(a);
+// // console.log(name);
+// // pi = 7
+// name = "Abdul Qudoos"
+//  a  = 8
+
+// let mYName = "Qudoos"
+// let my_name =  66
+// let name1 = "aa"
+// console.log(a);
+// console.log(name);
+// let city = "Karachi"
+// let residence = city
+// console.log(residence);
+// console.log("residence");
+
+// let std_name =  "john Doe"
+// let std_id = 3
+// let address =  "ABC Road"
+// let age = 50
+// let islinkedIn = true
+// let email 
+// console.log( "The Type of Email i:s",typeof std_id);
+// let score = "66"
+// console.log(typeof score);
+// score = Number(score)
+// console.log("The type of score id :",typeof score);
+// let text="57"
+// let textNum= Boolean(text)
+// console.log(typeof textNum);
+// console.log(textNum);
+
+// console.log(3>5);
+// console.log(3<5);
+// console.log(5=="5");
+// console.log(5==="5");
+
+// let radius = 3
+// let pi = 3.142
+// let area = pi*radius**2
+// console.log(area);
+
+if (3<5){
+    console.log("hello");
+
+}
+else{
+    console.log('overrated youtuber is the goat');
+    
+}
+
+
