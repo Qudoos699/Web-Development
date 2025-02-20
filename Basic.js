360d51d38e8111c068307a7a58fa9626cf45de47
Qudoos699 <qudoosabdul699@gmail.com>
2025-02-20 15:04:07 +0500

    Add files via upload

---
+++ Basic.js
@@ -0,0 +1,46 @@
+// let accountName= "musadiq"
+// var accountAddress= "ABC READ"
+// const email= "rsrony028@gmail.com"
+// let age= 18
+// city= "karachi"
+// let homeTown= city
+// let isalive= true
+
+// // console.log(acountName);
+
+// // accountName="Rohan"
+// // accountAddress="xyz"
+
+// // console.log(accountName);
+// // console.log(accountAddress);
+// // console.log(typeof homeTown);
+// // console.log("homeTown");
+
+
+// //logical Operator
+// console.log(3>5);
+// console.log(3<5);
+// console.log(3==5);
+// console.log(5==3);
+// console.log("4"==5);
+// console.log("5"===10);
+// strict compqriossion
+
+// // Arthematic Operator
+// const pi=3.142
+// let radius = 2
+// let area = pi * (radius **2) 
+// console.log("The area of given circle is = ",area);
+
+let a=1
+let b=6
+let even= 2
+let odd=1
+
+if(a==odd){
+    console.log(true);
+}
+
+else{
+    console.log(false);    
+}
